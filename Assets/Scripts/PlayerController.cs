fd0fe10031439913ae7bf91f3bd5db17389ece04
Christopher Soriano <2832@holbertonschool.com>
2022-01-05 11:53:46 -0400

    Add UI components to maze game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,15 +10,24 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private int score = 0;
     public int health = 5;
+    public Text scoreText;
+    public Text healthText;
+    public Text winLoseText;
+    public GameObject winLoseObject;
+    private Image winLoseBG;
 
     // Start is called before the first frame update
     void Update()
     {
         if (health == 0)
         {
-            Debug.Log("Game Over!");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SetLoseText();
+            StartCoroutine(LoadScene(3));
+        }
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("menu");
         }
     }
 
@@ -47,17 +57,52 @@ public class PlayerController : MonoBehaviour
         if (other.tag == "Pickup")
         {
             score++;
-            Debug.Log($"Score: {score}");
+            SetScoreText();
             Destroy(other.gameObject);
         }
         else if (other.tag == "Trap")
         {
             health--;
-            Debug.Log($"Health: {health}");
+            SetHealthText();
         }
         else if (other.tag == "Goal")
         {
-            Debug.Log("You win!");
+            SetWinText();
+            StartCoroutine(LoadScene(3));
         }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = $"Score: {score}";
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = $"Health: {health}";
+    }
+
+    void SetWinText()
+    {
+        winLoseBG = winLoseObject.GetComponent<Image>();
+        winLoseText.text = "You win!";
+        winLoseText.color = Color.black;
+        winLoseBG.color = Color.green;
+        winLoseObject.SetActive(true);
+    }
+
+    void SetLoseText()
+    {
+        winLoseBG = winLoseObject.GetComponent<Image>();
+        winLoseText.text = "Game Over!";
+        winLoseText.color = Color.white;
+        winLoseBG.color = Color.red;
+        winLoseObject.SetActive(true);
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
